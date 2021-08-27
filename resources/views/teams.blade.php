c2f965595520c324dbeacdf8f033b4dffcc987c1
Tuna <df95111111@gmail.com>
2021-08-27 11:07:34 +0800

    試做畫面雛形

---
+++ resources/views/teams.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.app')
+
+@section('body')
+    <div class="container">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">球隊</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">球員</a>
+            </li>
+        </ul>
+        <div class="row">
+            <div class="col-4 col-md-3 col-lg-2">
+                <img class="w-100" src="https://static.rti.org.tw/assets/thumbnails/2017/12/20/151373959668019.png" alt="道奇">
+            </div>
+            <div class="col-4 col-md-3 col-lg-2">
+                <img class="w-100" src="https://static.rti.org.tw/assets/thumbnails/2017/12/20/151373959668019.png" alt="道奇">
+            </div>
+            <div class="col-4 col-md-3 col-lg-2">
+                <img class="w-100" src="https://static.rti.org.tw/assets/thumbnails/2017/12/20/151373959668019.png" alt="道奇">
+            </div>
+        </div>
+
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/players', function () {
+    return view('players');
+});
+Route::get('/teams', function () {
+    return view('teams');
+});
